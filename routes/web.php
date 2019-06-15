0656abe7aa05b82c25e8864cbdad07fce8aec254
inoue shuri <tetsuand@gmail,com>
2019-06-15 23:39:09 +0000

    PHP/Laravel 09 Routingについて理解する

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/*========================================================================
+課題３はこちらにコメントの形で提出します。OKであれば次回提出の時には消します。
+
+http://XXXXXX.jp/XXX というアクセスが来たときに
+AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
+
+Route::get('XXX', 'YYY(Controllerディレクトリ名)\AAAController@bbb');
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create','Admin\NewsController@add');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
