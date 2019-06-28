80527c9e3089a5865b2c7937220323380afda94b
inoue shuri <tetsuand@gmail,com>
2019-06-28 22:50:42 +0000

    PHP/Laravel 13 ニュース投稿画面を作成しよう

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,16 +1,59 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
 @extends('layouts.profile')
-
-
-{{-- profile.blade.phpの@yield('title')に'プロフィールの作成'を埋め込む --}}
 @section('title', 'プロフィールの作成')
 
-{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>Profile Create</h2>
+                <h2>プロフィール新規作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    
+                    <!--　氏名　-->
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    
+                    
+                    <!--　性別　-->
+                    <div class="form-group row">
+                        <label class="col-md-2" for="gender">性別</label>
+                        <div class="col-md-10">
+                            <input type="checkbox" name="gender" value="男性">男性
+                            <input type="checkbox" name="gender" value="男性">女性
+                        </div>
+                    </div>
+                    
+                    <!--　趣味　-->
+                    <div class="form-group row">
+                        <label class="col-md-2" for="hobby">趣味</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby">
+                        </div>
+                    </div>
+                    
+                    <!--　自己紹介　-->
+                    <div class="form-group row">
+                        <label class="col-md-2" for="introduction">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="10"></textarea>
+                        </div>
+                    </div>
+                    
+                    
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
