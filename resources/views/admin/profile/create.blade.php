302f9a3c697c7990c4e35bd4d243f9fe7d32b45c
inoue shuri <tetsuand@gmail,com>
2019-06-29 01:29:29 +0000

    PHP/Laravel 14 投稿データを保存しよう

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -30,7 +30,7 @@
                         <label class="col-md-2" for="gender">性別</label>
                         <div class="col-md-10">
                             <input type="checkbox" name="gender" value="男性">男性
-                            <input type="checkbox" name="gender" value="男性">女性
+                            <input type="checkbox" name="gender" value="女性">女性
                         </div>
                     </div>
                     
